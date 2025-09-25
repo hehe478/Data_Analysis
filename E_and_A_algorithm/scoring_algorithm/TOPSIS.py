f52f3028f59d4174286e3598b625402ca925c7cd
lyh <liu1564684361@163.com>
2025-09-25 17:08:57 +0800

    optimize AHP and TOPSIS codes , add validation check section to improve the code robustness, polish the output

--- E_and_A_algorithm/scoring_algorithm/TOPSIS.py
+++ E_and_A_algorithm/scoring_algorithm/TOPSIS.py
@@ -1,96 +1,97 @@
 import numpy as np
-# Topsis法
 
-#  极小型转化
-def minTomax(maxx,x):
-    x = list(x)
-    x = [[maxx - i] for i in x]
-    return np.array(x)
+# TOPSIS法，用于计算得分的方法
 
-# 中间型转化
-def midTomax(midx,x):
-    x = list(x)
-    x = [abs(i - midx) for i in x]
-    M = max(x)
-    if M == 0:
-        M = 1
-    x = [[(1 - i / M)] for i in x]
-    return np.array(x)
+# 1.设置转化函数
+# 所有的参数传入请用np.array类型的数据请用
+# 所有的参数都需要注意是都有严重偏离的数据，若有需要修改下列函数防止对结果造成影响
+# 即因为若有严重偏离的数据，将会导致其他数据被压缩到一个极小的范围，无法显示差异
+# 在数据使用前进行数据预处理
+# 1.盖帽法  2.对数转化  3.删除
+# 1.1 极大型转化
+def to_max(x):  # 其实是毫无用处的一步，只是为了占位，代表这里有一个函数，方便以后如有需要进行修改
+    return x
+# 1.2 极小型转化
+def min_to_max(x):
+    max_x = np.max(x)
+    return max_x - x
+# 1.3 中间型转化
+def mid_to_max(x, best):
+    x = np.abs(x - best)
+    max_x = np.max(x)
+    if max_x == 0:
+        return np.ones_like(x)
+    return 1 - x / max_x
+# 1.4 区间型转化
+def reg_to_max(x, reg):
+    low_x, high_x = reg[0], reg[1]  # 取出范围，方便使用
+    # 使用&进行元素级逻辑与运算，并用括号明确优先级
+    mask_low = x < low_x
+    mask_high = x > high_x
+    mask_mid = ~mask_low & ~mask_high  # 既不小于下限也不大于上限
+    # 计算最大偏离值
+    deviations = np.concatenate([low_x - x[mask_low], x[mask_high] - high_x])
+    if len(deviations) == 0:  # 所有值都在区间内
+        return np.ones_like(x)
+    max_deviation = np.max(deviations)
+    # 对不同范围的数据进行处理
+    x_processed = np.zeros_like(x, dtype=float)
+    x_processed[mask_low] = 1 - (low_x - x[mask_low]) / max_deviation
+    x_processed[mask_high] = 1 - (x[mask_high] - high_x) / max_deviation
+    x_processed[mask_mid] = 1  # 区间内的值为1
+    return x_processed
 
-# 区间型转化
-def regTomax(lowx,highx,x):
-    x = list(x)
-    M = max(lowx - min(x), max(x) - highx)
-    if M == 0:
-        M = 1
-    ans = []
-    for i in range(len(x)):
-        if x[i] < lowx:
-            ans.append([(1 - (lowx - x[i]) / M)])
-        elif x[i] > highx:
-            ans.append([(1 - (x[i] - highx) / M)])
-        else:
-            ans.append([1])
-    return np.array(ans)
+
+# 2.输入矩阵，设置好评价参数
+matrix = np.array([[1, 2, 3, 4],
+                   [5, 6, 7, 8],
+                   [9, 10, 11, 12],
+                   [13, 14, 15, 16]])
+row_type = np.array([0, 1, 2, 3])  # 表示每一列的类型，区分数据进行的不同的转化
+row_best = [0, 0, 9, [13, 14]]  # 表示每一列需要的最佳数值或者区间，即使不需要，为了方便区分也进行了填充
 
 
-# 主函数调用
-print("请输入参评数目：") #例如3个人
-n = int(input()) #例如四项美德
-print("请输入指标数目：")
-m = int(input())
+# 3.处理矩阵，正向化处理
+m, n = matrix.shape  # 获取矩阵的形状
+processed_matrix = matrix.copy().astype(float)
 
-print("请输入类型矩阵：1.极大型 2.极小型 3.中间型 4.区间型")
-kind = input().split(" ")
+for i in range(n):
+    array = matrix[:, i]
+    if row_type[i] == 0:
+        processed_array = to_max(array)
+    elif row_type[i] == 1:
+        processed_array = min_to_max(array)
+    elif row_type[i] == 2:
+        processed_array = mid_to_max(array, row_best[i])
+    elif row_type[i] == 3:
+        processed_array = reg_to_max(array, row_best[i])
+    processed_matrix[:, i] = processed_array
 
-# 接受矩阵转化为numpy数组
-print("请输入矩阵：")
-matrix = np.array([list(map(float, input().split())) for _ in range(n)]).reshape(n, m)
-print(matrix)
+# 4.标准化处理
+for j in range(n):
+    col_sum_sq = np.sum(processed_matrix[:, j] ** 2)
+    if col_sum_sq != 0:
+        processed_matrix[:, j] = processed_matrix[:, j] / np.sqrt(col_sum_sq)
+# 如果全部都是0才能使col_sum_sq = 0,那个时候就不需要转化了
 
-# 统一指标，转化为极大型
-x = np.zeros(shape = (n,1))
-for i in range(m):
-    if kind[i] == "1":
-        v = np.array(matrix[:,i])
-    elif kind[i] == "2":
-        maxX = np.max(matrix[:,i])
-        v = minTomax(maxX , matrix[:,i])
-    elif kind[i] == "3":
-        print("类型三：请输入最优值")
-        maxX = eval(input())
-        v = midTomax(maxX , matrix[:,i])
-    elif kind[i] == "4":
-        print("类型四：请输入区间最小值")
-        lowA = eval(input())
-        print("类型四：请输入区间最大值")
-        highA = eval(input())
-        v = regTomax(lowA,highA,matrix[:,i])
-    if i == 0:
-        X = v.reshape(-1,1)
-    else:
-        X = np.hstack([X , v.reshape(-1,1)])
-print("统一后的矩阵为\n{}".format(X))
 
-# 对统一后的矩阵进行标准化处理
-X = X.astype(float)
-for j in range(m):
-    X[:,j] = X[:,j]/np.sqrt(sum(X[:,j]**2))
-print("标准化矩阵为\n{}".format(X))
+# 5.计算距离，统计得分
+# 计算理想解和负理想解
+ideal_best = np.max(processed_matrix, axis=0)  # 统计所有的研究对象中最优和最差，需要按行进行处理
+ideal_worst = np.min(processed_matrix, axis=0)
 
-# 计算最大最小距离
-maxD = np.max(X, axis=0)
-minD = np.min(X, axis=0)
-d_z = np.sqrt(np.sum(np.square((X - np.tile(maxD, (n, 1)))), axis=1))
-d_y = np.sqrt(np.sum(np.square((X - np.tile(minD, (n, 1)))), axis=1))
+# 计算到理想解和负理想解的距离
+d_best = np.sqrt(np.sum(np.square(processed_matrix - ideal_best), axis=1))  # 将每一个研究对象的各个项目和最优最差的数据进行对比，按列进行处理
+d_worst = np.sqrt(np.sum(np.square(processed_matrix - ideal_worst), axis=1))
 
-print("每个指标最大值：",maxD)
-print("每个指标最小值：",minD)
-print("d+向量：",d_z)
-print("d-向量：",d_y)
+print("每个指标理想最佳值：", ideal_best)
+print("每个指标理想最差值：", ideal_worst)
+print("到理想最佳解的距离：", d_best)
+print("到理想最差解的距离：", d_worst)
 
 # 计算得分
-s = d_y / (d_z + d_y)
-Score = 100 * s/sum(s)
-for i in range(len(Score)):
-    print("第{}个人得分为：{}".format(i+1,Score[i]))
+scores = d_worst / (d_best + d_worst)
+normalized_scores = 100 * scores / np.sum(scores)  # 转化为百分制
+
+for i in range(len(normalized_scores)):
+    print(f"第{i + 1}个评价对象得分为：{normalized_scores[i]:.2f}")
--- E_and_A_algorithm/weighting_algorthm/AHP.py
+++ E_and_A_algorithm/weighting_algorthm/AHP.py
@@ -17,6 +17,19 @@ A = np.array([[1,3,1,1/3],
 # *****************************************************************(其他代码具有复用性，无需修改)
 
 # 2.计算特征值和特征向量，找到最大特征值与其所对应的特征向量
+n , m = A.shape # 获取矩阵的维度，只是取了两个不同变量名，实际上m,n数值是一致的
+# 验证是否为正互反矩阵
+# 因为若是输入的矩阵过大或者复杂可能存在一定错误，所以进行一定判断保证输入矩阵的正确性
+for i in range(n):
+    if not np.isclose(A[i, i], 1):
+        raise ValueError(f"判断矩阵对角线元素必须为1，第{i + 1}行第{i + 1}列错误")
+    for j in range(i + 1, n):
+        if not np.isclose(A[i, j], 1 / A[j, i]):
+            raise ValueError(f"判断矩阵不满足互反性，第{i + 1}行第{j + 1}列与第{j + 1}行第{i + 1}列错误")
+    if np.any(A[i, :] <= 0):
+        raise ValueError(f"判断矩阵元素必须为正数，第{i + 1}行存在非正数")
+
+# 开始获取特征值与特征向量
 eigenvalues , eigenvectors = np.linalg.eig(A)  # 计算特征值和特征向量
 maxEigValIndex = np.argmax(eigenvalues)  # 最大特征值对应索引
 maxEigVal = eigenvalues[maxEigValIndex]  # 最大特征值
@@ -26,16 +39,22 @@ maxEigVec = eigenvectors[:,maxEigValIndex]  # 最大特征值所对应的特征�
 # CI = (maxEigVal - n）/（n - 1），最大特征值和矩阵的维度
 # CR = CI / RI
 # 如果CR小于0.1则通过，否则不通过
-n , m = A.shape # 获取矩阵的维度，只是取了两个不同变量名，实际上m,n数值是一致的
 CI = (maxEigVal - n)/(n - 1)
 CR = CI / RI[n]
 if CR > 0.1:
-    print(f"一致性检验不通过,值为{CR},本应小于0.1")
+    print(f"一致性检验不通过,值为{CR:.4f},本应小于0.1")
 else:
-    print(f"一致性检验通过，值为{CR},小于0.1")
+    print(f"一致性检验通过，值为{CR:.4f},小于0.1")
 
 # 4.计算权重
 # 就是为的最大特征值所对应的特征向量中的各个元素标准化了，计算各个的百分比
 sumVec = np.sum(maxEigVec)
 weight = maxEigVec / sumVec  # 计算权重
 print(weight)
+# 美化输出各项指标，但是不进行纯实数部分处理是为了，如果出现虚数部分能够及时发现问题
+print("\n=== 各指标权重 ===")
+indices = ["身高", "颜值", "学历", "性格"]  # 与判断矩阵列对应
+for i, (name, w) in enumerate(zip(indices, weight)):
+    print(f"{name}: 权重 = {w:.4f} ({w * 100:.2f}%)")
+
+print(f"\n权重总和: {np.sum(weight):.4f} (理论应为1)")
