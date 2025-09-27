348934539679a34a7bfaa521fe667dd8aa30bbbc
lyh <liu1564684361@163.com>
2025-09-27 23:21:57 +0800

    A bad statement today and just made EWM , I am a little confused why I code so hard

--- E_and_A_algorithm/weighting_algorthm/EWM.py
+++ E_and_A_algorithm/weighting_algorthm/EWM.py
@@ -1,28 +1,36 @@
 import numpy as np
 
-# TOPSIS法，用于计算得分的方法
+# 熵权法(Entropy Weight Method)，用于计算权重的方法
+
 
 # ******************************************************************************（可以修改的地方）
 
+
 # 1.设置转化函数
 # 所有的参数传入请用np.array类型的数据请用
+# 将数据进行正向化以及标准化以消除量纲的影响
+# 此处的标准化为最大最小范围内的放缩，如需要更严格的标准化，建议使用Z_Score标准化
+# Z_SCORE标准化后要保证数据全是正值，如有负值，建议加常数偏移
 # 所有的参数都需要注意是都有严重偏离的数据，若有需要修改下列函数防止对结果造成影响
 # 即因为若有严重偏离的数据，将会导致其他数据被压缩到一个极小的范围，无法显示差异
 # 在数据使用前进行数据预处理
 # 1.盖帽法  2.对数转化  3.删除
 # 1.1 极大型转化
-def to_max(x):  # 其实是毫无用处的一步，只是为了占位，代表这里有一个函数，方便以后如有需要进行修改
-    return x
+def to_max(x):  # 对数据进行标准化处理，压缩到0-1之间，保留信息的同时降低量纲差异
+    x_max = np.max(x)
+    x_min = np.min(x)
+    return (x - x_min)/(x_max - x_min)
 # 1.2 极小型转化
 def min_to_max(x):
     max_x = np.max(x)
-    return max_x - x
+    min_x = np.min(x)
+    return (max_x - x)/(max_x - min_x)
 # 1.3 中间型转化
 def mid_to_max(x, best):
     x = np.abs(x - best)
     max_x = np.max(x)
     if max_x == 0:
-        return np.ones_like(x)
+        return np.ones_like(x)  # 如果所有值都等于best，则都是最优，返回1
     return 1 - x / max_x
 # 1.4 区间型转化
 def reg_to_max(x, reg):
@@ -43,20 +51,19 @@ def reg_to_max(x, reg):
     x_processed[mask_mid] = 1  # 区间内的值为1
     return x_processed
 
-
-# 2.输入矩阵，设置好评价参数
-matrix = np.array([[1, 2, 3, 4],
-                   [5, 6, 7, 8],
-                   [9, 10, 11, 12],
-                   [13, 14, 15, 16]])
+# 2.输入评价矩阵
+matrix = np.array([[1,2,3,4],
+              [8,7,6,5],
+              [9,10,11,12],
+              [13,14,15,16]])
 row_type = np.array([0, 1, 2, 3])  # 表示每一列的类型，区分数据进行的不同的转化
 row_best = [0, 0, 9, [13, 14]]  # 表示每一列需要的最佳数值或者区间，即使不需要，为了方便区分也进行了填充
 
-# ******************************************************************************（其他代码可复用）
+
+# ******************************************************************************（其他地方可复用）
 
 
-
-# 3.处理矩阵，正向化处理
+# 3.对矩阵进行处理
 m, n = matrix.shape  # 获取矩阵的形状
 processed_matrix = matrix.copy().astype(float)
 
@@ -71,32 +78,25 @@ for i in range(n):
     elif row_type[i] == 3:
         processed_array = reg_to_max(array, row_best[i])
     processed_matrix[:, i] = processed_array
+# 4. 计算信息熵和权重
+# 加上一个极小值epsilon，防止log(0)的错误
+epsilon = 1e-10
 
-# 4.标准化处理
-for j in range(n):
-    col_sum_sq = np.sum(processed_matrix[:, j] ** 2)
-    if col_sum_sq != 0:
-        processed_matrix[:, j] = processed_matrix[:, j] / np.sqrt(col_sum_sq)
-# 如果全部都是0才能使col_sum_sq = 0,那个时候就不需要转化了
+# 计算比重 Pij，注意要使用 processed_matrix 并且按列(axis=0)求和
+P = processed_matrix / np.sum(processed_matrix, axis=0)
 
+# 计算每个指标的信息熵 e，结果是一个向量
+# 使用 P * np.log(P + epsilon) 来计算，np.sum按列(axis=0)求和
+k = -1 / np.log(m)
+e = k * np.sum(P * np.log(P + epsilon), axis=0)
 
-# 5.计算距离，统计得分
-# 计算理想解和负理想解
-ideal_best = np.max(processed_matrix, axis=0)  # 统计所有的研究对象中最优和最差，需要按行进行处理
-ideal_worst = np.min(processed_matrix, axis=0)
+# 计算信息冗余度 d (差异性系数)
+d = 1 - e
 
-# 计算到理想解和负理想解的距离
-d_best = np.sqrt(np.sum(np.square(processed_matrix - ideal_best), axis=1))  # 将每一个研究对象的各个项目和最优最差的数据进行对比，按列进行处理
-d_worst = np.sqrt(np.sum(np.square(processed_matrix - ideal_worst), axis=1))
+# 5. 计算最终权重
+# <<< 修正点: d.sum() 是对所有指标的差异性系数求和
+weight = d / np.sum(d)
 
-print("每个指标理想最佳值：", ideal_best)
-print("每个指标理想最差值：", ideal_worst)
-print("到理想最佳解的距离：", d_best)
-print("到理想最差解的距离：", d_worst)
-
-# 计算得分
-scores = d_worst / (d_best + d_worst)
-normalized_scores = 100 * scores / np.sum(scores)  # 转化为百分制
-
-for i in range(len(normalized_scores)):
-    print(f"第{i + 1}个评价对象得分为：{normalized_scores[i]:.2f}")
+formatted_weights = [f"{w:.4f}" for w in weight]  # 规范格式输出
+print(f"各指标的权重为: {formatted_weights}")
+print("\n权重加和:", np.sum(weight))
